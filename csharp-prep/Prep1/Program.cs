6e842620b8764ef536aab95fa6c689d0ab3b0f79
LordLawson <lawsoniwuagwu47@gmail.com>
2024-09-24 22:56:15 +0100

    Added wk-1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello my name is World!");
+
+        Console. Write("What is your first Name? ");
+         string firstName = Console.ReadLine ();
+
+          Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {firstName}, Is nice to meet you ...({lastName} {firstName} )");
+
+
+        
     }
+
+
 }
